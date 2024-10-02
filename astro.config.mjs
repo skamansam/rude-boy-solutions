a6ab022998241c48ca1485a93a474c76cbec4879
Samuel C. Tyler <skamansam@gmail.com>
2024-10-02 00:18:48 -0400

    add github workflow for deployments

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import svelte from "@astrojs/avelte";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  integrations: [avelte(), react(), tailwind()],
+  base: "rude-boy-solutions",
+  site: "https://rudeboy.dev/rude-boy-solutions",
 });
