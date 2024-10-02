07e922a9f2bad873f2a98804a89310cacbc81553
Samuel C. Tyler <skamansam@gmail.com>
2024-10-02 09:38:43 -0400

    convert brnads to svelte

---
+++ src/components/Brands.svelte
@@ -0,0 +1,67 @@
+<script lang="ts">
+    import { M as motion } from "svelte-motion";
+
+    import AmazonLogo from "../assets/logos/AmazonLogo.svelte";
+    import DropboxLogo from "../assets/logos/DropboxLogo.svelte";
+    import NetflixLogo from "../assets/logos/NetflixLogo.svelte";
+    import SlackLogo from "../assets/logos/SlackLogo.svelte";
+    import SpotifyLogo from "../assets/logos/SpotifyLogo.svelte";
+    import StripeLogo from "../assets/logos/StripeLogo.svelte";
+</script>
+
+<section class="py-12 sm:py-24 bg-bgDark1 w-full lg:mt-16 mb-8 lg:mb-16">
+    <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        viewport={{ once: true }}
+        transition={{ duration: 0.5, delay: 0.2 }}
+    >
+        <div
+            class="container px-4 mx-auto 2xl:w-[1200px] xl:w-[1100px] lg:w-[1000px] md:w-4/5"
+        >
+            <div
+                class="flex lg:flex-row flex-col items-center -mx-4 justify-center lg:text-left text-center"
+            >
+                <div class="w-full lg:w-1/2 px-4 mb-12 lg:mb-0">
+                    <div class="flex flex-col">
+                        <h2
+                            class="mb-2 text-4xl sm:text-5xl 2xl:text-6xl font-bold tracking-normal text-primaryText"
+                        >
+                            Trusted by brands
+                        </h2>
+                        <h2
+                            class=" text-4xl sm:text-5xl 2xl:text-6xl font-bold tracking-normal text-secondaryColor"
+                        >
+                            all over the world
+                        </h2>
+                    </div>
+                </div>
+                <div
+                    class="w-2/3 sm:w-[620px] lg:w-1/2 mx-auto lg:mx-0 lg:pl-10"
+                >
+                    <div class="flex flex-wrap -m-4">
+                        <div class="w-1/2 sm:w-1/3 py-6 flex justify-center">
+                            <AmazonLogo />
+                        </div>
+                        <div class="w-1/2 sm:w-1/3 py-6 flex justify-center">
+                            <DropboxLogo />
+                        </div>
+                        <div class="w-1/2 sm:w-1/3 py-6 flex justify-center">
+                            <NetflixLogo />
+                        </div>
+                        <div class="w-1/2 sm:w-1/3 py-6 flex justify-center">
+                            <StripeLogo />
+                        </div>
+
+                        <div class="w-1/2 sm:w-1/3 py-6 flex justify-center">
+                            <SpotifyLogo />
+                        </div>
+                        <div class="w-1/2 sm:w-1/3 py-6 flex justify-center">
+                            <SlackLogo />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </motion.div>
+</section>
