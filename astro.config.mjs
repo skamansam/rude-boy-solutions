3c6c61cd475791d8eaf9fa2930a7183dfaf8e1c4
Samuel C. Tyler <skamansam@gmail.com>
2024-10-02 00:32:43 -0400

    add svelte to astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-import svelte from "@astrojs/avelte";
+import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  integrations: [avelte(), react(), tailwind()],
+  integrations: [svelte(), react(), tailwind()],
   base: "rude-boy-solutions",
   site: "https://rudeboy.dev/rude-boy-solutions",
 });
